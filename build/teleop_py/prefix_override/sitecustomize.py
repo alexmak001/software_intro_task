9fd490241fa038730b99433dc6c825f7101d9a7e
Alex <alexm7701@gmail.com>
2022-06-30 16:28:21 -0700

    edit

---
+++ build/teleop_py/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/alex/tritonai/software_intro_task/install/teleop_py'
---
+++ teleop_py/build/teleop_py/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/alex/tritonai/software_intro_task/teleop_py/install/teleop_py'
